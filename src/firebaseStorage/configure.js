3130386e78a7316c327b173430997c140ab52a67
devknight216 <devknight216@gmail.com>
2021-08-13 20:47:10 +0300

    Firebase configuration

--- src/firebaseStorage/configure.js
+++ src/firebaseStorage/configure.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 
 // Use your config values here.
 firebase.initializeApp({
-    apiKey: "AIzaSyBJ3eUPfCm64Wft6x6eVIkfFOwqzPzLkww",
-    authDomain: "legendary-3d210.firebaseapp.com",
-    projectId: "legendary-3d210",
-    storageBucket: "legendary-3d210.appspot.com",
-    messagingSenderId: "932435775002",
-    appId: "1:932435775002:web:9adda74653680169840ba3",
-    measurementId: "G-FFW7C9WH6E"
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCHET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
 });
 
 export default firebase
