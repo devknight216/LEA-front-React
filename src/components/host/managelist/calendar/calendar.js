36d29e882bdf5bc2fefa7ba84918620f19644c15
devknight216 <devknight216@gmail.com>
2021-09-05 05:21:00 +0300

    Add calendar

--- src/components/host/managelist/calendar/calendar.js
+++ src/components/host/managelist/calendar/calendar.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+require('react-big-calendar/lib/css/react-big-calendar.css');
 
-function ManageListCalendarComponent() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const localizer = momentLocalizer(moment)
 
-export default ManageListCalendarComponent
+const ManageListCalendarComponent = props => (
+  <div className="my-16">
+    <Calendar
+      localizer={localizer}
+      events={[]}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+)
+
+export default ManageListCalendarComponent;
