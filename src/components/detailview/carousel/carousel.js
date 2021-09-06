297d88bbfa46ba043d848fa1baae6e861071c0d5
devknight216 <devknight216@gmail.com>
2021-09-06 22:24:18 +0300

    Image View

--- src/components/detailview/carousel/carousel.js
+++ src/components/detailview/carousel/carousel.js
@@ -1,45 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ImageViewComponent from './imageview';
 
 function DetailViewCarouselComponent({images}) {
-    return (
-      <div className="mx-auto container">
-        {
-          images && 
-          <div className="mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
-            <div className="hidden aspect-w-3 aspect-h-4 rounded-lg overflow-hidden lg:block">
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="mx-auto container">
+      {
+        images && <div className="mt-6 max-w-7xl mx-auto cursor-pointer" onClick={()=>{setIsOpen(true)}}>
+          <div className="grid grid-cols-1 md:grid-cols-3 md:gap-8">
+            <div className="hidden rounded-lg overflow-hidden lg:block">
               <img
                 src={images[0]?.url}
                 alt={images[0]?.filename}
                 className="w-full h-full object-center object-cover"
               />
             </div>
-            <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
-              <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden h-72">
+            <div className="col-span-2 space-y-6">
+              <div className="rounded-lg overflow-hidden px-4 sm:px-0 h-72">
                 <img
                   src={images[1]?.url}
                   alt={images[1]?.filename}
                   className="w-full h-full object-center object-cover"
                 />
               </div>
-              <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden h-72">
-                <img
-                  src={images[2]?.url}
-                  alt={images[2]?.filename}
-                  className="w-full h-full object-center object-cover"
-                />
+              <div className="rounded-lg overflow-hidden px-4 sm:px-0 h-72 hidden md:block">
+                <div className="grid grid-cols-1 md:grid-cols-3 h-full md:gap-x-4 ">
+                  <div className="hidden rounded-lg overflow-hidden lg:block">
+                    <img
+                      src={images[2]?.url}
+                      alt={images[2]?.filename}
+                      className="w-full h-full object-center object-cover"
+                    />
+                  </div>
+                  <div  className="hidden rounded-lg overflow-hidden lg:block">
+                    <img
+                      src={images[3]?.url}
+                      alt={images[3]?.filename}
+                      className="w-full h-full object-center object-cover"
+                    />
+                  </div>
+                  <div  className="hidden rounded-lg overflow-hidden lg:block">
+                    <img
+                      src={images[4]?.url}
+                      alt={images[5]?.filename}
+                      className="w-full h-full object-center object-cover"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4">
-              <img
-                src={images[3]?.url}
-                alt={images[3]?.filename}
-                className="w-full h-full object-center object-cover"
-              />
-            </div>
           </div>
-        }
-      </div>
-    )
+        </div>
+      }
+      {
+        images && <ImageViewComponent isOpen={isOpen} closeViewer={()=>{setIsOpen(false)}} images={images}/>
+      }
+    </div>
+  )
 }
 
 export default DetailViewCarouselComponent
---
+++ src/components/detailview/carousel/imageview.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import ImgsViewer from "react-images-viewer";
+
+export default function ImageViewComponent({images, isOpen, closeViewer}){
+    console.log(images);
+    const [currImg, setCurrImg] = useState(0);
+    const gotoPrev = () => {
+        if(currImg !== 0){
+            setCurrImg(currImg - 1);
+        }
+    }
+
+    const gotoNext = () => {
+        if(true){
+            setCurrImg(currImg + 1);
+        }
+    }
+
+    const [imageArray, setImageArray ] = useState([]);
+    useEffect(() => {
+        const imageArray = [];
+        images.forEach(element => (
+            setImageArray(prev => [...prev, {src:element.url}])
+        ));
+    }, [])
+
+    return (
+        <ImgsViewer
+            imgs={imageArray}
+            isOpen={isOpen}
+            currImg={currImg}
+            onClickPrev={ gotoPrev }
+            onClickNext={gotoNext}
+            onClose={closeViewer}
+        />
+    )
+}
