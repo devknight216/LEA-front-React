20f17cc3d741544c252d33bb1dfbb3c515ae98ad
devknight216 <devknight216@gmail.com>
2021-09-16 20:52:35 +0300

    Add host section on Detail View

--- src/views/detailview/propertydetailview.jsx
+++ src/views/detailview/propertydetailview.jsx
@@ -20,6 +20,7 @@ const PropertyDetailViewPage = (props) => {
     dispatch(getPropertyById(props.match.params.id));
   }, []);
   const property = useSelector((state) => state.properties.property);
+  console.log(property);
   const stats = [
     { label: "Guests", value: property?.guestNum, icon: GuestIcon },
     { label: "Bedrooms", value: property?.bedroomNum, icon: BedroomIcon },
@@ -42,6 +43,32 @@ const PropertyDetailViewPage = (props) => {
             <div className="grid grid-cols-1 gap-4 items-start lg:grid-cols-3 lg:gap-8">
               {/* Left column */}
               <div className="grid grid-cols-1 gap-4 lg:col-span-2">
+                <section>
+                  <div className="px-4 py-2 w-100">
+                    <div>
+                      <p className="text-gray-600 text-lg font-bold">Host</p>
+                    </div>
+                    <div className="grid grid-cols-1 sm:grid-cols-4">
+                      <div className="col-span-1 flex justify-center">
+                        {property?.host?.avatarURL ? (
+                          <div>
+                            <img src={property.host.avatarURL} className="h-20 w-20 rounded-full border-2 object-cover" />
+                          </div>
+                        ) : (
+                          <span className="h-20 w-20 rounded-full overflow-hidden bg-gray-100">
+                            <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                            </svg>
+                          </span>
+                        )}
+                      </div>
+                      <div className="col-span-3 py-2">
+                        <p className="text-gray-600 text-lg font-bold">{property?.host?.name}</p>
+                        <p className="text-gray-600 text-sm font-bold">{property?.host?.email}, {property?.host?.phone}</p>
+                      </div>
+                    </div>
+                  </div>
+                </section>
                 {/* Welcome panel */}
                 <section aria-labelledby="profile-overview-title">
                   <div className="rounded-lg bg-white overflow-hidden shadow">
